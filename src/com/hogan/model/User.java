aa3d4ea96a226074820cab47242f3b897fbaef14
HoganHu0520 <hhlsch@hotmail.com>
2015-04-19 16:30:28 +0800

    Apply annotation hibernate.
    Apply annotation hibernate.

--- src/com/hogan/model/User.java
+++ src/com/hogan/model/User.java
@@ -1,13 +1,27 @@
 package com.hogan.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.hogan.model.base.BaseModel;
 
+@Entity
+@Table(name = "t_user")
 public class User extends BaseModel {
 	private String name;
 	private String password;
 	
 	public User(){}
+	
+	@Override
+	@Id
+	public Integer getId() {
+		return super.id;
+	}
 
+	@Column(name = "name", length = 45)
 	public String getName() {
 		return name;
 	}
@@ -16,6 +30,7 @@ public class User extends BaseModel {
 		this.name = name;
 	}
 
+	@Column(name = "password", length = 16)
 	public String getPassword() {
 		return password;
 	}
