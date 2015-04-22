353e68fb50fd7cb1c2db396d039ff7aa6183d920
HoganHu0520 <hhlsch@hotmail.com>
2015-04-22 11:39:06 +0800

    Apply SpringMVC
    Apply SpringMVC.
    Added WEB-INF.

---
+++ src/com/hogan/model/Book.java
@@ -0,0 +1,78 @@
+package com.hogan.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.hogan.model.base.BaseModel;
+
+@Entity
+@Table(name = "t_book")
+public class Book extends BaseModel{
+	private String title;
+	private String author;
+	private String discription;
+	private Double amount;
+	private Integer count;
+	private String imageUrl;
+	
+	@Override
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int getId() {
+		return super.getId();
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public String getDiscription() {
+		return discription;
+	}
+	
+	public void setDiscription(String discription) {
+		this.discription = discription;
+	}
+	
+	public Double getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	
+	public Integer getCount() {
+		return count;
+	}
+	
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+	
+	@Column(name = "image_url")
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
+}
